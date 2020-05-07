234c25b5658ca7bd033668f5bf36d054ccd68206
Seth Sacher <ssacher@collabralink.com>
2020-05-07 10:22:32 -0400

    Test change to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -9,16 +9,18 @@ class App extends Component {
   render() {
     return (
       <Router>
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Fib Calculator version 2</h1>
-            <Link to="/">Home</Link>
-            <Link to="/otherpage">Other Page</Link>
+        <div className='App'>
+          <header className='App-header'>
+            <img src={logo} className='App-logo' alt='logo' />
+            <h1 className='App-title'>
+              Fib Calculator version test-deployment
+            </h1>
+            <Link to='/'>Home</Link>
+            <Link to='/otherpage'>Other Page</Link>
           </header>
           <div>
-            <Route exact path="/" component={Fib} />
-            <Route path="/otherpage" component={OtherPage} />
+            <Route exact path='/' component={Fib} />
+            <Route path='/otherpage' component={OtherPage} />
           </div>
         </div>
       </Router>
